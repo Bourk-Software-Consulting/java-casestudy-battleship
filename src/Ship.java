8119ec9501f9438863cd709636491397a4b489be
Simon Bourk <sbourk@gmail.com>
2020-01-06 12:32:58 -0500

    boyscout on Ship

--- src/Ship.java
+++ src/Ship.java
@@ -4,83 +4,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-/// <summary>
-/// The ship.
-/// </summary>
-public class Ship
+class Ship
 {
-    protected Character code = 'S';
-
-    /// <summary>
-    /// Gets  the name.
-    /// </summary>
-    protected String name;
-
-    /// <summary>
-    /// The color of the ship
-    /// </summary>
+    Character code = 'S';
+    private String name;
     private  Color color;
     private ISea sea;
+    private Integer size;
+    ArrayList<Position> positions = new ArrayList<Position>();
 
-    private Boolean isPlaced;
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="com.bourksoftware.Ship"/> class.
-    /// </summary>
-    public Ship(String name, Integer size, java.awt.Color color, ISea boardgame)
+    Ship(String name, Integer size, java.awt.Color color, ISea boardgame)
     {
         this.name = name;
         this.size = size;
         this.color = color;
         this.sea = boardgame;
-        positions = new ArrayList<Position>();
     }
-    public Ship(String name, int size, Color color) {
+
+    Ship(String name, int size, Color color) {
         this(name,size,color,null);
     }
-    /// <summary>
-    /// Gets the positions.
-    /// </summary>
-    public ArrayList<Position> positions;
 
-
-    /// <summary>
-    /// Gets or sets the size.
-    /// </summary>
-    private Integer size;
-
-
-
-
-    /// <summary>
-    /// The add position.
-    /// </summary>
-    /// <param name="input">
-    /// The input.
-    /// </param>
-    public void AddPosition(String input)
+    private void AddPosition(String input)
     {
-        if (positions == null)
-        {
-            positions = new ArrayList<Position>();
-        }
-
         Letters letter = Enum.valueOf(Letters.class, input.toUpperCase().substring(0, 1));
-
-        Integer number = Integer.parseInt(input.substring(1, 2));
+        int number = Integer.parseInt(input.substring(1, 2));
         positions.add(new Position(letter,  number));
     }
 
-    public Boolean IsPlaced() {
-        return isPlaced;
-    }
-    public void setIsPlaced(Boolean value)
-    {
-            if (value.equals(isPlaced)) return;
-            isPlaced = value;
-    }
-
-    public void place() throws IOException {
+    void place() {
         System.out.println();
         System.out.println("Please enter the position for the Front of the Ship with its orientation (Horizontal (H) or Vertical (V) for the "+ this.name +" (size: "+ size+")" );
         System.out.println("example: A4H = will result in placing the ship at Coordinates: A4 A5 A6 A7 A8");
@@ -99,7 +51,7 @@ public class Ship
         }
     }
 
-    public void placeAt(String positionAndOrientation){
+    void placeAt(String positionAndOrientation){
 
         AddPosition(positionAndOrientation.substring(0,2));
 
